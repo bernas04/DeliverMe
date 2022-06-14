bb1cf342c10e2ab6eb060444609e37e12f3f28f5
bernas04 <joaobernardo0@ua.pt>
2022-06-15 00:01:14 +0100

    little update

--- webapp/src/pages/CourierDetails/index.js
+++ webapp/src/pages/CourierDetails/index.js
@@ -41,35 +41,45 @@ const CourierDetails = () => {
     return(
         <Container>
             <Link className="btn btn-primary" to="/couriers">Back</Link>
-            <h2 className="mt-3">Courier #{params.courierId}: {/* rider.name */}</h2>
-
-            <dl className="fs-4">
-                <dt>Information:</dt>
-                <dd>Total deliveries {rider.totalReviews}</dd>
-                <dd>Average reviews: {rider.averageReview}</dd>
- 
-
-                <dt>Other bit of info:</dt>
-                <dd>Another value</dd>
-            </dl>
-
-            <h3>Delivering:</h3>
-            <ListGroup>
-                <ListGroup.Item>a delivery</ListGroup.Item>
-            </ListGroup>
-
-          {
-                hasPastDeliveries ?
-                <div>
-                    <h3>Past Deliveries:</h3>
-                    <ListGroup>
-                        <ListGroup.Item>Past Deliveries</ListGroup.Item>
-                    </ListGroup>
-                </div>
-                :
-                <h3>No Past Deliveries</h3>
-            }
             
+            {
+                rider == null ?
+                <h3>Loading...</h3>
+                :
+                fetchError ?
+                <h3>An error ocorred while fetching data</h3>
+                :
+                <>
+                <h2 className="mt-3">Courier #{params.courierId}: {/* rider.name */}</h2>
+
+                <dl className="fs-4">
+                    <dt>Information:</dt>
+                    <dd>Total deliveries {rider.totalReviews}</dd>
+                    <dd>Average reviews: {rider.averageReview}</dd>
+    
+
+                    <dt>Other bit of info:</dt>
+                    <dd>Another value</dd>
+                </dl>
+
+                <h3>Delivering:</h3>
+                <ListGroup>
+                    <ListGroup.Item>a delivery</ListGroup.Item>
+                </ListGroup>
+
+                    {
+                        hasPastDeliveries ?
+                        <div>
+                            <h3>Past Deliveries:</h3>
+                            <ListGroup>
+                                <ListGroup.Item>Past Deliveries</ListGroup.Item>
+                            </ListGroup>
+                        </div>
+                        :
+                        <h3>No Past Deliveries</h3>
+                    }
+                </>
+            }
         </Container>
     )
 
