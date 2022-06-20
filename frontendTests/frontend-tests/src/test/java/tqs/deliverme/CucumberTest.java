15897e5be3fea86995b0dd207d333a0585b88051
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2022-06-20 23:16:54 +0100

    Update dockers

---
+++ frontendTests/frontend-tests/src/test/java/tqs/deliverme/CucumberTest.java
@@ -0,0 +1,16 @@
+package tqs.deliverme;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("tqs/deliverme")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tqs.deliverme")
+public class CucumberTest {
+
+}
---
+++ frontendTests/frontend-tests/src/test/java/tqs/deliverme/ViewOngoingSteps.java
@@ -0,0 +1,21 @@
+package tqs.deliverme;
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+*/
+public class ViewOngoingSteps {
+
+   //private WebDriver driver;
+
+   
+
+}
